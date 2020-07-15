cf62c4e42a7faaaec25cda2a202385d26b6b60d7
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-07-14 18:56:23 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const repoContext = require("./repository/repository-wrapper.js");
+const express = require('express');
+const app = express();
+
+app.listen(3000, function(){
+    console.log("Server started. Listening on port 3000.");
+});
