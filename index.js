f6e83355ddb9d4a098574c4ebe6c553a68af1a4b
david rubio <rubdo@davids-Air.attlocal.net>
2020-07-14 21:40:20 -0500

    add get endpoint

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const app = express();
 app.listen(3000, function(){
     console.log("Server started. Listening on port 3000.");
 });
+console.log(repoContext)
+app.get("/api/products", (req, res) => {
+    let products = repoContext.movies.findAllMovies();
+    res.send(products);
+   });
