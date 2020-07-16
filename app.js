2c6d0be0db671608f74671c3c46d7802c072f897
david rubio <rubdo@davids-Air.attlocal.net>
2020-07-16 17:52:25 -0500

    add get MovieByID function

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ function getAllMovies(){
     });
 }
 getAllMovies();
+
+function getMovieByID(movieID){
+    $.ajax({
+        method: "GET", 
+        url: "http://localhost:3000/api/products/" + movieID, 
+        data: {}, 
+        async: false, 
+    }).done(function( data ) {
+        console.log('Yay it works!', data)
+    })
+}
