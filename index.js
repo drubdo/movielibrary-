28de993410c485f1f8f94fa6a846791e15a9613f
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-07-22 07:26:42 -0700

    Fixed indentation

--- index.js
+++ index.js
@@ -5,37 +5,39 @@ const express = require("express");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use((req, res, next) => validators.body(req, res, next));
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
 });
-console.log(repoContext)
+
 app.get("/api/products", (req, res) => {
     let products = repoContext.movies.findAllMovies();
     res.send(products);
-   });
+});
 
-   app.get("/api/products/:id", (req, res) => {
+app.get("/api/products/:id", (req, res) => {
     let id = req.params.id;
     let product = repoContext.movies.findMovieById(id);
     res.send(product);
-   });
+});
 
-   app.post("/api/products/create", (req, res) => {
+app.post("/api/products/create", (req, res) => {
     let newProduct = req.body;
     let addedProduct = repoContext.movies.createMovie(newProduct);
     res.send(addedProduct);
-   });
+});
 
-   app.put("/api/products/updateMovie", (req, res) => {
+app.put("/api/products/updateMovie", (req, res) => {
     let productToUpdate = req.body;
     let updatedProduct = repoContext.movies.updateMovie(productToUpdate);
     res.send(updatedProduct);
-   });
-   app.delete("/api/products/deleteMovie/:id", (req, res) => {
+});
+app.delete("/api/products/deleteMovie/:id", (req, res) => {
     let id = req.params.id;
     let updatedDataSet = repoContext.movies.deleteMovie(id);
     res.send(updatedDataSet);
-   });
+});
